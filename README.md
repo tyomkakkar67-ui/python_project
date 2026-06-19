aa5396cf527a70892073409360aabff1e115085d
tyomkakkar67-ui <tyomkakkar67@gmail.com>
2026-06-19 17:50:37 +0530

    because i need this
    hello

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # python_project
-learning  through about python
--- new.py
+++ new.py
@@ -1,1 +1,5 @@
 print("Hello, World!")
+print("welcome to python programming")
+print("third number is greatest");  
+print("hello")
+print("what is the greatest number?")
